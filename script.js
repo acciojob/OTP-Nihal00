c89599baab6308796a1af5e83e159a7962455e14
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-28 12:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+// Get the input elements
+const inputs = document.querySelectorAll('.code');
+
+// Listen for keyup events on the inputs
+inputs.forEach(input => {
+  input.addEventListener('keyup', () => {
+    // If the key pressed is the backspace key
+    if (event.keyCode === 8) {
+      // Remove the last character from the input
+      input.value = input.value.slice(0, -1);
+
+      // If the input is empty
+      if (input.value === '') {
+        // Focus the previous input
+        const previousInput = input.previousElementSibling;
+        if (previousInput) {
+          previousInput.focus();
+        }
+      }
+    } else {
+      // Otherwise, add the character to the input
+      input.value += event.keyCode;
+
+      // If the input is full
+      if (input.value.length === 6) {
+        // Focus the verify button
+        document.getElementById('verify-button').focus();
+      } else {
+        // Focus the next input
+        const nextInput = input.nextElementSibling;
+        if (nextInput) {
+          nextInput.focus();
+        }
+      }
+    }
+  });
+});
